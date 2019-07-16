e1663880967d251c83b951a3d41d433422edc24b
kuritka <kuritka@gmail.com>
2019-07-16 10:38:25 +0000

    merge files

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
-import "testDb/db"
+import (
+        "database/sql"
+        _ "github.com/lib/pq"
+        "fmt"
+        "log"
+
+)
+
 
 func main() {
-	db.ReadSomeData()
+	ReadSomeData()
+}
+
+
+var db *sql.DB
+
+
+//this function is executed before rest of the functionality is called
+func init(){
+        cs := "postgres://..."
+        var err error
+        db, err = sql.Open("postgres",  cs)
+
+        if err != nil {
+                panic(err.Error())
+        }
+}
+
+
+func ReadSomeData() error {
+        rows, err := db.Query("SELECT id, issuer  FROM  edt_issuers")
+        FailOnError(err,"querying db")
+        for rows.Next() {
+                var id int
+                var issuer string
+                err = rows.Scan(&id, &issuer)
+                FailOnError(err, "reading sensor from DB")
+                fmt.Printf("%v %s", id, issuer)
+        }
+        return err
+}
+
+
+func FailOnError(err error, msg string){
+        if err != nil {
+                log.Fatalf("%s: %s", msg, err)
+                panic(fmt.Sprintf("%s: %s", msg, err))
+        }
 }
 
 
